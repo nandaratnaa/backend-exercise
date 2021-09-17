1e6e377589bb9aadb4b6c7a6a449bdba8f311d1e
nandaratnaa <nnd.rtna@gmail.com>
2021-09-17 15:52:40 +0200

    add files

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
 const express = require("express");
 const app = express();
-const PORT = 4002;
+const PORT = 4000;
 
-app.listen(PORT, () => console.log("Hey server running at 4002"));
+const teamRouter = require("./routers/team");
+const playerRouter = require("./routers/player");
+const userRouter = require("./routers/user");
+
+ 
+//register middleware
+app.use(express.json());
+
+//register routers
+app.use("/teams", teamRouter);
+app.use("/players", playerRouter);
+app.use("/users", userRouter);
+
+
+
+app.listen(PORT, () => console.log("Hey server running at 4000"));
